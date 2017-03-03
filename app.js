a3b710e777424f71754d662fce1d30fcd8f6fd4a
thetongz <miiz_tongz@live.com>
2017-03-03 17:59:41 +0700

    almost done

--- app.js
+++ app.js
@@ -15,6 +15,34 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/html/index.html")
 })
 
+app.get("/", function(req, res) {
+    var name = req.body.name
+    var time = req.body.timeuse
+    fs.readFile(__dirname + "/public/data/ranking.json", "utf-8", function(err, data) {
+        var json = JSON.parse(data)
+        if (req.body.level == "easy") {
+            json.easy.push({
+                name: name,
+                time: time
+            })
+        } else if (req.body.level == "medium") {
+            json.medium.push({
+                name: name,
+                time: time
+            })
+        } else {
+            json.hard.push({
+                name: name,
+                time: time
+            })
+        }
+        var nj = JSON.stringify(json)
+        fs.writeFile(__dirname + "/public/data/ranking.json", nj, 'utf8', function(err, data) {
+            res.sendFile(__dirname + "/public/html/index.html")
+        })
+    })
+})
+
 app.post("/menu", function(req, res) {
     if (req.body.mode == "play")
         res.sendFile(__dirname + "/public/html/level.html")
