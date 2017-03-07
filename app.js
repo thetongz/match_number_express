79eee66b4bd6cd029e6b00f4345f21c895ba1934
thetongz <miiz_tongz@live.com>
2017-03-08 01:05:46 +0700

    set all route in app to route file

--- app.js
+++ app.js
@@ -3,57 +3,18 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var fs = require('fs')
 var app = express()
+var index = require('./routes/index')
+var morgan = require('morgan')
+
 
 app.set('views engine', 'pug')
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
-
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/html/index.html")
-})
-
-app.post("/", function(req, res) {
-    var name = req.body.name
-    var time = req.body.timeuse
-    fs.readFile(__dirname + "/public/data/ranking.json", "utf-8", function(err, data) {
-        var json = JSON.parse(data)
-        if (req.body.level == "easy") {
-            json.easy.push({
-                name: name,
-                time: time
-            })
-        } else if (req.body.level == "medium") {
-            json.medium.push({
-                name: name,
-                time: time
-            })
-        } else {
-            json.hard.push({
-                name: name,
-                time: time
-            })
-        }
-        var nj = JSON.stringify(json)
-        fs.writeFile(__dirname + "/public/data/ranking.json", nj, 'utf8', function(err, data) {
-            res.sendFile(__dirname + "/public/html/index.html")
-        })
-    })
-})
-
-app.post("/menu", function(req, res) {
-    if (req.body.mode == "play")
-        res.sendFile(__dirname + "/public/html/level.html")
-    else if (req.body.mode == "ranking") {
-        fs.readFile(__dirname + "/public/data/ranking.json", "utf-8", function(err, data) {
-            var json = JSON.parse(data)
-            res.render("ranking.pug", { data: json })
-        })
-    } else
-        res.sendFile(__dirname + "/public/html/howto.html")
-})
+app.use('/', index)
 
 app.listen(3000, function() {
     console.log("Running at PORT 3000")
---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express')
+var path = require('path')
+var fs = require('fs')
+var route = express.Router()
+
+route.get("/", function(req, res) {
+    res.sendFile(path.resolve(__dirname + "/../public/html/index.html"))
+})
+
+route.post("/", function(req, res) {
+    var name = req.body.name
+    var time = (req.body.timeuse % 60) + ":" + Math.floor(req.body.timeuse / 60) + " min"
+    fs.readFile(path.resolve(__dirname + "/../public/data/ranking.json"), "utf-8", function(err, data) {
+        var json = JSON.parse(data)
+        if (req.body.level == "easy") {
+            json.easy.push({
+                name: name,
+                time: time
+            })
+        } else if (req.body.level == "medium") {
+            json.medium.push({
+                name: name,
+                time: time
+            })
+        } else {
+            json.hard.push({
+                name: name,
+                time: time
+            })
+        }
+        var nj = JSON.stringify(json)
+        fs.writeFile(path.resolve(__dirname + "/../public/data/ranking.json"), nj, 'utf8', function(err, data) {
+            res.sendFile(path.resolve(__dirname + "/../public/html/index.html"))
+        })
+    })
+})
+
+route.post("/menu", function(req, res) {
+    if (req.body.mode == "play")
+        res.sendFile(path.resolve(__dirname + "/../public/html/level.html"))
+    else if (req.body.mode == "ranking") {
+        fs.readFile(path.resolve(__dirname + "/../public/data/ranking.json"), "utf-8", function(err, data) {
+            var json = JSON.parse(data)
+            res.render("ranking.pug", { data: json })
+        })
+    } else
+        res.sendFile(path.resolve(__dirname + "/../public/html/howto.html"))
+})
+
+route.all('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname + "/../public/html/index.html"))
+})
+
+module.exports = route
