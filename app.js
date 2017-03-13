fb1fd627f1c88e44365af265721ed8890f8bbafe
thetongz <miiz_tongz@live.com>
2017-03-13 15:26:23 +0700

    add process.env.PORT

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
-var fs = require('fs')
 var app = express()
 var index = require('./routes/index')
 var morgan = require('morgan')
 
-
+app.set('views', path.join(__dirname, '/views'))
 app.set('views engine', 'pug')
 app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname, '/public')))
@@ -16,6 +15,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.use('/', index)
 
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT, function() {
     console.log("Running at PORT 3000")
 })
